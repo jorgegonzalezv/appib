c08f31be5c5b9a56df87ebbdc650a1254228a164
jorge <jorge.gonzalezv95@gmail.com>
2021-03-14 20:29:27 +0100

    [update] de todo un poco

--- src/model.py
+++ src/model.py
@@ -1,22 +1,38 @@
 import torch
 from torch import nn
 
+device = "cpu" # no tengo cuda ni nvidia :(
+
 class NeuralNetwork(nn.Module):
 
     def __init__(self):
         super(NeuralNetwork, self).__init__()
-        self.flatten = nn.Flatten()
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(28*28, 512),
+        #self.flatten = nn.Flatten()
+        self.convolutional = nn.Sequential(
+            nn.Conv2d(3, 6, 6),
             nn.ReLU(),
-            nn.Linear(512, 512),
+            nn.MaxPool2d(3,3),
+            nn.Conv2d(6, 16, 6),
             nn.ReLU(),
-            nn.Linear(512, 10),
-            nn.ReLU()
+            nn.MaxPool2d(3,3)
+            )
+
+        self.linear = nn.Sequential(
+            nn.Linear(16 * 26 * 26, 128),
+            nn.ReLU(),
+            nn.Linear(128, 32),
+            nn.ReLU(),
+            nn.Linear(32, 16),
+            nn.ReLU(),
+            nn.Linear(16,1),
+            nn.Sigmoid(),
         )
 
     def forward(self, x):
-        x = self.flatten(x)
-        logits = self.linear_relu_stack(x)
-        return logits
+        x = self.convolutional(x)
+        #print(x.shape)
+        x = x.view(-1, 16 * 26 * 26)
+        prob = self.linear(x)
+        prob = prob.permute(1,0).squeeze().float()
+        return prob 
 
--- src/train.py
+++ src/train.py
@@ -1,5 +1,11 @@
+"""
+    Training code based on 
+    https://pytorch.org/tutorials/beginner/basics/quickstart_tutorial.html
+"""
+
 import torch 
-from model import NeuralNetwork
+from torch import nn
+from model import NeuralNetwork, device
 from torchvision.datasets import ImageFolder
 from torchvision import transforms
 from torch.utils.data import DataLoader
@@ -14,9 +20,11 @@ def train(dataloader, model, loss_fn, optimizer):
     size = len(dataloader.dataset)
     for batch, (X, y) in enumerate(dataloader):
         X, y = X.to(device), y.to(device)
-
+        y= y.float()
         # Compute prediction error
         pred = model(X)
+        #print("pred:", pred)
+        #print("target:",y)
         loss = loss_fn(pred, y)
 
         # Backpropagation
@@ -28,31 +36,74 @@ def train(dataloader, model, loss_fn, optimizer):
             loss, current = loss.item(), batch * len(X)
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
+def test(dataloader, model, loss_fn, threshold=0.5):
+    size = len(dataloader.dataset)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            y = y.float()
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += ((pred > threshold) == y).type(torch.float).sum().item()
+    test_loss /= size
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
 
 if __name__ == '__main__':
 
-    # data loader
-    t_ = transforms.Compose([
+    epochs = 1
+    batch_size = 4
+    resize = (256, 256)
+
+    # TODO: normalizar colores, resize mas pequeño... etc
+    # train data loader
+    t_train = transforms.Compose([
             transforms.RandomRotation(180),
             transforms.ToTensor(),
+            transforms.Resize(resize) #,interpolation='bilinear'),
             ])
 
     train_path = "Task_1/development"
-    train_ds = ImageFolder(train_path, transform=t_)
-
-
-    train_ds_loader = DataLoader(train_ds)
+    train_ds = ImageFolder(train_path, transform=t_train)
+    train_ds_loader = DataLoader(train_ds, batch_size=batch_size, shuffle=True)
 
     #for im, tag in train_ds_loader:
-    #    print(tag)
+    #    print(im.shape)
+    #    plt.imshow(im[0].permute(1,2,0))
+    #    plt.show()
+    #    break
+
+    # evaluation (test) dataloader
+    t_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Resize(resize)# ,interpolation=transforms.InterpolationMode.BILINEAR),
+        ])
+    test_path = "Task_1/evaluation"
+    test_ds = ImageFolder(test_path,transform=t_test)
+    test_ds_dataloader = DataLoader(test_ds, batch_size=batch_size)
 
     # model
     model = NeuralNetwork()
-
+    print(model)
+    pytorch_total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    print("Numero de parametros: " ,pytorch_total_params)
+    
     # model loss
-    loss_fn = nn.CrossEntropyLoss()
+    # cross-entropy, ya que vamos a predecir probabilidades
+    # de clasificar en fake o no fake
+    loss_fn = nn.BCELoss()
 
     # optimizer
     optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
 
+    for e in range(epochs):
+        print(f"Epoch {e+1}\n-------------------------------")
+        train(train_ds_loader, model, loss_fn, optimizer)
+        test(test_ds_dataloader, model, loss_fn)
+
+    torch.save(model.state_dict(), "weights/model.pth")
+    print("Saved PyTorch Model State to weights/model.pth")
 
